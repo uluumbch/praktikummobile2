49aee95a021f81a85dcaa84e796a4b82d91c75f1
uluumbch <uluum123@gmail.com>
2022-04-12 23:27:08 +0800

    menambahkan folder modul 3
    berisi projeka aplikasi konverter mata uang

---
+++ modul3/konvertermatauang/app/src/main/java/com/uluumbch/konvertermatauang/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.uluumbch.konvertermatauang
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.uluumbch.konvertermatauang.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tombolKonversi.setOnClickListener{ hitungKonversi() }
+
+//        menerapkan fungsi untuk menyembunyikan keyboard
+        binding.inputEditText.setOnKeyListener{ view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )}
+    }
+
+
+    private fun  hitungKonversi(){
+        val stringInput = binding.inputEditText.text.toString()
+        val uangInput = stringInput.toDoubleOrNull()
+
+        if (uangInput == null){
+            binding.teksHasil.text = ""
+            return
+        }
+
+        val pilihanUser = when (binding.pilihanMataUang.checkedRadioButtonId){
+            R.id.euro -> 15617
+            R.id.USD -> 14355
+            R.id.JPY -> 114
+            else -> 3827
+        }
+
+        var cetakHasil =  uangInput * pilihanUser
+
+        val indonesianLocale = Locale("in", "ID")
+        val hasil_format =
+            NumberFormat.getCurrencyInstance(indonesianLocale).format(cetakHasil)
+
+        binding.teksHasil.text = getString(R.string.hasil, hasil_format)
+    }
+
+
+//    fungsi untuk menyembunyikan keyboard
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
